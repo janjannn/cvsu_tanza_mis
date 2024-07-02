1ec495ea5f85ed00b37b8952864a1b464b8bb834
John <johnzunigacastillo@gmail.com>
2024-07-02 13:29:11 +0800

    add migrations for dtr

---
+++ database/migrations/2024_07_01_083205_create_dtrs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Enums\TimePeriod;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDtrsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dtrs', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->date('date');
+            $table->timestamps();
+
+            $table->unique(['user_id', 'date']);
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dtrs');
+    }
+}
